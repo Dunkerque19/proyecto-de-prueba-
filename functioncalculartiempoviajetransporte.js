eb9b1bf50ccd13e6cc56eda5d95e21b4b3deef07
Dunkerque19 <163428980+Dunkerque19@users.noreply.github.com>
2024-04-04 12:55:20 -0300

    Add files via upload

--- functioncalculartiempoviajetransporte.js
+++ functioncalculartiempoviajetransporte.js
@@ -1,3 +1,23 @@
+function calculartiempoviajetransporte(distancia, velocidadpromedio, transoorte) {
+    if(transoorte == 'auto'){
+        velocidadpromedio =100;
+        return distancia / velocidadpromedio;
+
+    }
+    if(transoorte == 'bicicleta'){
+        velocidadpromedio =15;
+        return distancia / velocidadpromedio;
+    }
+    }
+
+
+
+
+// ejemplo de uso 
+const distancia= 600 // en kilometros 
+const velocidadpromedio= 80; // en kilometros por hora 
+const tiempoviaje= calculartiempoviajetransporte(distancia, velocidadpromedio);
+console.log("el tiempo de viaje es: "+ tiempoviaje + "hors");
 
 
 
